65150701b37441709441b21ba3056d0c4395b99c
DikeshReddy <dikeshreddy3637@gmail.com>
2025-11-28 20:15:40 +0530

    added data.sql file to seed test users

---
+++ backend/auth-service/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO users (
+    user_id,
+    user_name,
+    email,
+    password,
+    is_admin,
+    created_at,
+    updated_at
+) VALUES
+(
+    '060e7771-b5dc-4fd6-bebf-fd165a075b6d',
+    'dikesh',
+    'dikesh@gmail.com',
+    '$2a$10$FhWyVP5UG4zhHHZZVLiWK.gTyl1IPmNud4/ZQKElGBJbM1rCT2NHu',
+    TRUE,
+    '2025-11-28 18:57:21.6683',
+    '2025-11-28 18:57:21.66835'
+),
+(
+    '0a2f5569-7348-4c9f-b20f-886a50f896bd',
+    'nithin',
+    'nithin@gmail.com',
+    '$2a$10$alSBhhRq8eHbgMYY7U0tvevUXmlcdvwPprZ9y3SEoxlh5Ha3yzrle',
+    FALSE,
+    '2025-11-28 19:01:33.628005',
+    '2025-11-28 19:01:33.628029'
+);
